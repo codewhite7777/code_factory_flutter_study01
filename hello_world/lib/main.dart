685a8d7d48141f3d572db215e0b03d22c9548e43
alee <codewhite7777@gmail.com>
2022-11-03 01:07:09 +0900

    feat: backgroundColor, text Color & Size

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -3,8 +3,14 @@ import 'package:flutter/material.dart';
 void main() => runApp(
       const MaterialApp(
         home: Scaffold(
+          backgroundColor: Colors.black,
           body: Center(
-            child: Text('Hello world'),
+            child: Text(
+              'Hello world',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
           ),
         ),
       ),
