e36075488463362f2d731418bfbfa531fb857119
alee <alee@aleeui-MacBookPro.local>
2022-11-20 23:36:01 +0900

    feat: number image

--- random_number_generator/lib/screen/home_screen.dart
+++ random_number_generator/lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:random_number_generator/constant/color.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,44 +9,81 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<int> numList = [123, 456, 789];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: PRIMARY_COLOR,
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text('랜덤 숫자 생성기'),
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.settings),
-                ),
-              ],
-            ),
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text('123'),
-                  Text('456'),
-                  Text('789'),
+                  const Text(
+                    '랜덤 숫자 생성기',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  IconButton(
+                    color: RED_COLOR,
+                    onPressed: () {},
+                    icon: const Icon(Icons.settings),
+                  ),
                 ],
               ),
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: const Text('생성하기'),
-                  ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: numList
+                      .asMap()
+                      .entries
+                      .map(
+                        (entry) => Row(
+                          children: entry.value
+                              .toString()
+                              .split('')
+                              .map(
+                                (x) => Padding(
+                                  padding: EdgeInsets.only(
+                                      bottom: (entry.key == 2) ? 0 : 16.0),
+                                  child: Image.asset(
+                                    'asset/img/$x.png',
+                                    height: 70.0,
+                                    width: 50.0,
+                                  ),
+                                ),
+                              )
+                              .toList(),
+                        ),
+                      )
+                      .toList(),
                 ),
-              ],
-            ),
-          ],
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: RED_COLOR,
+                      ),
+                      onPressed: () {
+                        debugPrint('click');
+                      },
+                      child: const Text('생성하기'),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
