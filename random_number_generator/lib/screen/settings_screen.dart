21c8add366638a17f004aa918a8818ae17dbc7f3
alee <alee@aleeui-MacBookPro.local>
2022-11-21 12:27:09 +0900

    release: random_number_generator

--- random_number_generator/lib/screen/settings_screen.dart
+++ random_number_generator/lib/screen/settings_screen.dart
@@ -30,51 +30,93 @@ class _SettingsScreenState extends State<SettingsScreen> {
           padding: const EdgeInsets.symmetric(horizontal: 16.0),
           child: Column(
             children: [
-              Expanded(
-                child: Row(
-                  children: settingNumber
-                      !.toInt()
-                      .toString()
-                      .split('')
-                      .map(
-                        (e) => Image.asset(
-                          'asset/img/$e.png',
-                          width: 50.0,
-                          height: 70.0,
-                        ),
-                      )
-                      .toList(),
-                ),
-              ),
-              Slider(
-                min: minNumber,
-                max: maxNumber,
-                value: settingNumber!,
-                onChanged: (double value) {
-                  setState(() {
-                    settingNumber = value;
-                  });
-                },
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: RED_COLOR,
-                      ),
-                      onPressed: () {
-                        Navigator.of(context).pop(settingNumber!.toInt());
-                      },
-                      child: const Text('저장'),
-                    ),
-                  ),
-                ],
+              _Body(settingNumber: settingNumber!),
+              _Fotter(
+                maxNumber: maxNumber,
+                minNumber: minNumber,
+                settingNumber: settingNumber!,
+                onSliderChanged: onSliderChanged,
               ),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  void onSliderChanged(double value) {
+    setState(() {
+      settingNumber = value;
+    });
+  }
+}
+
+class _Body extends StatelessWidget {
+  final double settingNumber;
+
+  const _Body({required this.settingNumber, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Row(
+        children: settingNumber!
+            .toInt()
+            .toString()
+            .split('')
+            .map(
+              (e) => Image.asset(
+                'asset/img/$e.png',
+                width: 50.0,
+                height: 70.0,
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
+}
+
+class _Fotter extends StatelessWidget {
+  final double minNumber;
+  final double maxNumber;
+  final double settingNumber;
+  final ValueChanged<double> onSliderChanged;
+
+  const _Fotter(
+      {required this.minNumber,
+      required this.maxNumber,
+      required this.settingNumber,
+      required this.onSliderChanged,
+      Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Slider(
+          min: minNumber,
+          max: maxNumber,
+          value: settingNumber!,
+          onChanged: onSliderChanged,
+        ),
+        Row(
+          children: [
+            Expanded(
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: RED_COLOR,
+                ),
+                onPressed: () {
+                  Navigator.of(context).pop(settingNumber!.toInt());
+                },
+                child: const Text('저장'),
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
