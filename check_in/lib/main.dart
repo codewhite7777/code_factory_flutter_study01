c51f09e88b4945e8b5e819425f7d0ecfe062debb
alee <alee@aleeui-MacBookPro.local>
2022-11-27 03:46:03 +0900

    release: check_in

--- check_in/lib/main.dart
+++ check_in/lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:check_in/screen/home_screen.dart';
 
-void  main()=>runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomeScreen(),);
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
   }
 }
--- check_in/lib/screen/home_screen.dart
+++ check_in/lib/screen/home_screen.dart
@@ -10,14 +10,14 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  static final LatLng companyLatLng = LatLng(37.524327, 126.921252);
+  static final LatLng companyLatLng = const LatLng(37.524327, 126.921252);
   static final CameraPosition initialPosition = CameraPosition(
     target: companyLatLng,
     zoom: 15.0,
   );
   static final double okDistance = 100.0;
   static final Circle withinDistanceCircle = Circle(
-    circleId: CircleId("withinDistanceCircle"),
+    circleId: const CircleId("withinDistanceCircle"),
     center: companyLatLng,
     fillColor: Colors.blue.withOpacity(0.5),
     radius: okDistance,
@@ -25,7 +25,7 @@ class _HomeScreenState extends State<HomeScreen> {
     strokeWidth: 1,
   );
   static final Circle notWithinDistanceCircle = Circle(
-    circleId: CircleId("notWithinDistanceCircle"),
+    circleId: const CircleId("notWithinDistanceCircle"),
     center: companyLatLng,
     fillColor: Colors.red.withOpacity(0.5),
     radius: okDistance,
@@ -33,7 +33,7 @@ class _HomeScreenState extends State<HomeScreen> {
     strokeWidth: 1,
   );
   static final Circle checkDoneCircle = Circle(
-    circleId: CircleId("checkDoneCircle"),
+    circleId: const CircleId("checkDoneCircle"),
     center: companyLatLng,
     fillColor: Colors.green.withOpacity(0.5),
     radius: okDistance,
@@ -41,9 +41,11 @@ class _HomeScreenState extends State<HomeScreen> {
     strokeWidth: 1,
   );
   static final Marker marker = Marker(
-    markerId: MarkerId('marker'),
+    markerId: const MarkerId('marker'),
     position: companyLatLng,
   );
+  bool checkInDone = false;
+  GoogleMapController? mapController;
 
   @override
   Widget build(BuildContext context) {
@@ -56,46 +58,68 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ),
         backgroundColor: Colors.white,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.my_location),
+            color: Colors.blue,
+            onPressed: () async {
+              if (this.mapController == null) {
+                return;
+              }
+              final Position location = await Geolocator.getCurrentPosition();
+              mapController!.animateCamera(
+                CameraUpdate.newLatLng(
+                  LatLng(location.latitude, location.longitude),
+                ),
+              );
+            },
+          ),
+        ],
       ),
       body: SafeArea(
         child: FutureBuilder<String>(
           future: checkPermission(),
           builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
             if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(child: CircularProgressIndicator());
+              return const Center(child: CircularProgressIndicator());
             }
             if (snapshot.data == '이 앱의 위치 권한이 허가되었습니다.') {
               return StreamBuilder<Position>(
-                  stream: Geolocator.getPositionStream(),
-                  builder:
-                      (BuildContext context, AsyncSnapshot<Position> snapshot) {
-                    bool isWithinRange = false;
-                    if (snapshot.hasData) {
-                      final start = snapshot.data;
-                      final end = companyLatLng;
-                      final distance = Geolocator.distanceBetween(
-                          start!.latitude,
-                          start!.longitude,
-                          end.latitude,
-                          end.longitude);
-                      if (distance < okDistance) {
-                        isWithinRange = true;
-                      }
+                stream: Geolocator.getPositionStream(),
+                builder:
+                    (BuildContext context, AsyncSnapshot<Position> snapshot) {
+                  bool isWithinRange = false;
+                  if (snapshot.hasData) {
+                    final start = snapshot.data;
+                    final end = companyLatLng;
+                    final distance = Geolocator.distanceBetween(start!.latitude,
+                        start!.longitude, end.latitude, end.longitude);
+                    if (distance < okDistance) {
+                      isWithinRange = true;
                     }
-                    return Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        _CustomGoogleMap(
-                          initialPosition: initialPosition,
-                          circle: (isWithinRange == false)
-                              ? notWithinDistanceCircle
-                              : withinDistanceCircle,
-                          marker: marker,
-                        ),
-                        _CheckIn(),
-                      ],
-                    );
-                  });
+                  }
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      _CustomGoogleMap(
+                        initialPosition: initialPosition,
+                        circle: (checkInDone)
+                            ? checkDoneCircle
+                            : (isWithinRange == false)
+                                ? notWithinDistanceCircle
+                                : withinDistanceCircle,
+                        marker: marker,
+                        onMapCreated: onMapCreated,
+                      ),
+                      _CheckIn(
+                        isWithinRange: isWithinRange,
+                        onPressed: onCheckPressed,
+                        checkInDone: checkInDone,
+                      ),
+                    ],
+                  );
+                },
+              );
             }
             return Center(
               child: Text(snapshot.data!),
@@ -104,6 +128,41 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       ),
     );
+  }
+
+  void onMapCreated(GoogleMapController controller) {
+    this.mapController = controller;
+  }
+
+  void onCheckPressed() async {
+    final result = await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Check In'),
+          content: const Text('Check In?'),
+          actions: [
+            TextButton(
+              child: const Text('No'),
+              onPressed: () {
+                Navigator.of(context).pop(false);
+              },
+            ),
+            TextButton(
+              child: const Text('Yes'),
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+            ),
+          ],
+        );
+      },
+    );
+    if (result == true) {
+      setState(() {
+        checkInDone = true;
+      });
+    }
   }
 
   Future<String> checkPermission() async {
@@ -130,12 +189,14 @@ class _CustomGoogleMap extends StatelessWidget {
   final CameraPosition initialPosition;
   final Circle circle;
   final Marker marker;
+  final MapCreatedCallback onMapCreated;
 
   const _CustomGoogleMap({
     Key? key,
     required this.initialPosition,
     required this.circle,
     required this.marker,
+    required this.onMapCreated,
   }) : super(key: key);
 
   @override
@@ -149,19 +210,50 @@ class _CustomGoogleMap extends StatelessWidget {
         mapType: MapType.normal,
         circles: Set.from([circle]),
         markers: Set.from([marker]),
+        onMapCreated: onMapCreated,
       ),
     );
   }
 }
 
 class _CheckIn extends StatelessWidget {
-  const _CheckIn({Key? key}) : super(key: key);
+  final bool isWithinRange;
+  final VoidCallback onPressed;
+  final bool checkInDone;
+
+  const _CheckIn({
+    Key? key,
+    required this.isWithinRange,
+    required this.onPressed,
+    required this.checkInDone,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: Center(
-        child: Text('google map'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.timelapse_outlined,
+              size: 50,
+              color: (checkInDone)
+                  ? Colors.green
+                  : (isWithinRange)
+                      ? Colors.blue
+                      : Colors.red,
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            if (isWithinRange && (checkInDone == false))
+              TextButton(
+                onPressed: onPressed,
+                child: const Text('Check In'),
+              ),
+          ],
+        ),
       ),
     );
   }
